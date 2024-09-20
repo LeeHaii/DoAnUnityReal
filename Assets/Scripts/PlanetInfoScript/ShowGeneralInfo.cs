68e452978769cae5672b55435f7fb27a948219c0
LeeHaii <thangthcskt@gmail.com>
2024-09-20 21:37:13 +0700

    chit
    ưda

--- Assets/Scripts/PlanetInfoScript/ShowGeneralInfo.cs
+++ Assets/Scripts/PlanetInfoScript/ShowGeneralInfo.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using SimpleJSON;
+using Palmmedia.ReportGenerator.Core.Common;
+using System.IO;
 
 public class ShowGeneralInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private class Planet
+    {
+        public string planetName;
+        public long planetMass;
+        public float planetRaidus;
+        public double dFS;
+        public float orbitalPeriod;
+    }
+
+    [Header("Attributes")]
+    [SerializeField]
+    private TMP_Text planetName;
+    [SerializeField]
+    private TMP_Text planetMass;
+    [SerializeField]
+    private TMP_Text dFS;
+    [SerializeField]
+    private TMP_Text orbitalPeriod;
+    [SerializeField]
+    private string textColor;
+    private string jsonPath = File.ReadAllText("./../PlanetInfo/GeneralInfo.json");
+
     void Start()
     {
-        
+        List<Planet> planets = JsonSerializer.Deserialize<List<Planet>>(jsonPath);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void GetJsonData()
+    {
+        print("Get Json Data");
+    }
+    public void SetPlanetText()
+    {
+
+    }
+    
 }
